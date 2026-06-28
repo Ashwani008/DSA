5c8af620e0a4c05fa9ec838dd9235fcad38bbd27
Ashwani008 <55152445+Ashwani008@users.noreply.github.com>
2026-06-28 18:02:56 +0530

    Time: 0 ms (100.00%) | Memory: 29.7 MB (44.02%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,5 +1,13 @@
 class Solution {
 public:
+
+    void RotatebyOne(vector<int>& nums, int n) {
+        int temp = nums[n-1];
+        for(int i =n-1; i>=1; i--) {
+            nums[i] = nums[i-1];
+        }
+        nums[0] =  temp;
+    }
     void Reverse(vector<int>& nums, int s, int e) {
         int l = s, r = e;
         while(l <= r) {
@@ -20,7 +28,9 @@ public:
         Reverse(nums, 0, n-1);
         Reverse(nums, 0, k-1);
         Reverse(nums, k, n-1);
-        
+        // for(int i =0; i<k; i++)
+        //     RotatebyOne(nums, n);
+
         return;
     }
 };
