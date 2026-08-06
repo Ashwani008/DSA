43cf9a904b13f8c732c713fb5894c79e793a092e
Ashwani008 <55152445+Ashwani008@users.noreply.github.com>
2026-08-06 21:59:44 +0530

    Time: 113 ms (5.39%) | Memory: 24.1 MB (25.34%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -2,17 +2,15 @@ class NumArray {
 public:
     vector<int> arr;
     NumArray(vector<int>& nums) {
-        arr.resize(nums.size());
-        arr[0] = nums[0];
-        for(int i=1; i<nums.size(); i++) {
-            arr[i] =arr[i-1]+nums[i];
-        }
+        arr = nums;
     }
     
     int sumRange(int left, int right) {
-        if(left == 0)
-            return arr[right];
-        return (arr[right] - arr[left-1]);
+        int sum = 0;
+        for(int i = left; i<=right; i++) {
+            sum += arr[i];
+        }
+        return sum;
     }
 };
 
