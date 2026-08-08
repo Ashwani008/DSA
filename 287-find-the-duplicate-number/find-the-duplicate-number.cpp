31fe034a7c1c5a9244b68c6479b3fe412ccb9555
Ashwani008 <55152445+Ashwani008@users.noreply.github.com>
2026-08-08 16:58:28 +0530

    Time: 3 ms (68.54%) | Memory: 67.6 MB (24.16%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
+        // int n = nums.size();
+
+        // int sl = nums[0];
+        // int ft = nums[0];
+
+        // sl = nums[sl];
+        // ft = nums[nums[ft]];
+
+        // while(sl != ft) {
+        //     sl = nums[sl];
+        //     ft = nums[nums[ft]];
+
+        // }
+
+        // sl = nums[0];
+        // while(sl != ft) {
+        //     sl = nums[sl];
+        //     ft = nums[ft];
+        // }
+
+        // return sl;
         int n = nums.size();
-
-        int sl = nums[0];
-        int ft = nums[0];
-
-        sl = nums[sl];
-        ft = nums[nums[ft]];
-
-        while(sl != ft) {
-            sl = nums[sl];
-            ft = nums[nums[ft]];
-
+        vector<int> vis(n+1, 0);
+        for(int i =0; i<n; i++) {
+            if(vis[nums[i]] == -1){
+                return nums[i];
+            }
+            vis[nums[i]] = -1;
         }
-
-        sl = nums[0];
-        while(sl != ft) {
-            sl = nums[sl];
-            ft = nums[ft];
-        }
-
-        return sl;
+        return -1;
     }
 };
