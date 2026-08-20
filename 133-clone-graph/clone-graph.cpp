a818568d31b7db75aefba3b0abf208192f5f5160
Ashwani008 <55152445+Ashwani008@users.noreply.github.com>
2026-08-20 23:29:48 +0530

    Time: 3 ms (68.20%) | Memory: 11.9 MB (91.12%) - LeetSync

--- 133-clone-graph/clone-graph.cpp
+++ 133-clone-graph/clone-graph.cpp
@@ -23,32 +23,62 @@ class Solution {
 public:
     unordered_map<Node*, Node*> mp;
 
-    void DFS(Node* old, Node* newNode) {
+    // void DFS(Node* old, Node* newNode) {
 
-        for(auto n : old->neighbors) {
+    //     for(auto n : old->neighbors) {
 
-            if(mp.find(n) == mp.end()) {
-                Node* newclone = new Node(n->val);
-                mp[n] = newclone;
+    //         if(mp.find(n) == mp.end()) {
+    //             Node* newclone = new Node(n->val);
+    //             mp[n] = newclone;
 
-                newNode->neighbors.push_back(newclone);
-                DFS(n, newclone);
-            } else {
-                newNode->neighbors.push_back(mp[n]);
-            }
-        }
-        return;
+    //             newNode->neighbors.push_back(newclone);
+    //             DFS(n, newclone);
+    //         } else {
+    //             newNode->neighbors.push_back(mp[n]);
+    //         }
+    //     }
+    //     return;
 
-    }
+    // }
+
+    // Node* cloneGraph(Node* node) {
+    //     if(node == NULL)
+    //         return node;
+    //     mp.clear();
+    //     Node* newNode = new Node(node->val);
+    //     mp[node] = newNode;
+        
+    //     DFS(node, newNode);
+    //     return newNode;
+    // }
 
     Node* cloneGraph(Node* node) {
         if(node == NULL)
             return node;
-        mp.clear();
+
         Node* newNode = new Node(node->val);
         mp[node] = newNode;
-        
-        DFS(node, newNode);
+
+        queue<Node*> q;
+        q.push(node);
+
+        while(!q.empty()) {
+            Node* n = q.front();
+            Node* newclone = mp[n];
+            q.pop();
+            
+            for(auto it : n->neighbors) {
+                if(mp.find(it) == mp.end()) {
+                    Node * cl = new Node(it->val);
+                    mp[it] = cl;
+                    q.push(it);
+                    newclone->neighbors.push_back(cl);
+                } else {
+                    newclone->neighbors.push_back(mp[it]);
+                }
+            }
+
+        }
         return newNode;
     }
 };
