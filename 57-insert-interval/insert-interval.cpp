9ce2103aa0660fb5d3fe19ebeb6a474c8c0d73c7
Ashwani008 <55152445+Ashwani008@users.noreply.github.com>
2026-08-23 13:28:09 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (76.59%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -2,40 +2,23 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>> ans;
-        if(intervals.size() == 0){
-            vector<int> temp(newInterval);
-            ans.push_back(temp);
-            return ans;
+        int n = intervals.size(), i=0;
+
+        while(i < n && intervals[i][1] < newInterval[0]){
+            ans.push_back(intervals[i]);
+            i++;
         }
         
-        int newele = newInterval[0];
-        int findindex = intervals.size();
-        for(int i = 0; i<intervals.size(); i++) {
-            int st = intervals[i][0];
-            if(st > newele){
-                findindex = i;
-                break;
-            }
+        while(i<n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min(intervals[i][0], newInterval[0]);
+            newInterval[1] = max(intervals[i][1], newInterval[1]);
+            i++;
         }
-        
-        intervals.insert(intervals.begin() + findindex, newInterval);
-
-
-        int st = intervals[0][0];
-        int en = intervals[0][1];
-    
-        
-        for(int i = 1; i<intervals.size(); i++){
-
-            if(en >= intervals[i][0]){
-                en = max(en, intervals[i][1]);
-            } else {
-                ans.push_back({st, en});
-                st = intervals[i][0];
-                en = intervals[i][1];
-            }
+        ans.push_back(newInterval);
+        while(i < n) {
+            ans.push_back(intervals[i]);
+            i++;
         }
-        ans.push_back({st, en});
         return ans;
     }
 };
