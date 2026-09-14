ee8b84aea3e2d5ca78d1701b8e6bd752d8277044
Ashwani008 <55152445+Ashwani008@users.noreply.github.com>
2026-09-14 07:49:47 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (9.94%) - LeetSync

--- 228-summary-ranges/summary-ranges.cpp
+++ 228-summary-ranges/summary-ranges.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
+        int n = nums.size(),i=0;
         vector<string> ans;
-        
-        for(int i =0;i<nums.size(); i++) {
-            int st = nums[i];
-
-            while((i+1 < nums.size()) && (nums[i] + 1 == nums[i+1]) ) {
+        string s="";
+        while(i<n){
+            int st=nums[i];
+            while(i+1 < n && nums[i]+1 == nums[i+1]){
                 i++;
             }
-
-            if(st == nums[i]) {
+            if(st == nums[i]){
                 ans.push_back(to_string(st));
             } else {
                 ans.push_back(to_string(st) + "->" + to_string(nums[i]));
             }
-
+            i++;
         }
         return ans;
     }
