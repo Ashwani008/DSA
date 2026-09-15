6fe821a102b2cd539674beba1ea16ce2ed791bcb
Ashwani008 <55152445+Ashwani008@users.noreply.github.com>
2026-09-15 22:50:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (67.27%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto ch  : s) {
+            if(ch == '(' || ch == '{' || ch == '[')
+                st.push(ch);
+            else{
+                if(st.empty())
+                    return false;
+                char t = st.top();
+                if(t == '(' && ch == ')' || t == '{' && ch == '}' || t == '[' && ch == ']' ){
+                    st.pop();
+                } else{
+                    return false;
+                }
+            }
+        }
+        if(st.empty())
+            return true;
+        else
+            return false;
+    }
+};
